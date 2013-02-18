b834f44ae43b0ab91188f66c8108eecbd105a452
Alessandro Galli <ag6609@doc.ic.ac.uk>
2013-02-18 20:31:09 +0000

    grounded function, oh it's a one liner!

--- q1.pl
+++ q1.pl
@@ -1,8 +1,15 @@
+
 argument(a).
 argument(b).
 argument(c).
+argument(d).
 
+attacks(a,a).
 attacks(a,b).
-attacks(b,c).
+attacks(b,a).
+attacks(c,d).
+attacks(d,c).
 
 admissible(Y) :- not(attacks(Y,Y)), (not(attacks(X,Y)); (attacks(Y,Z), X==Z)).
+
+grounded(Z) :- argument(Z), forall((argument(X)), (\+(attacks(X,Z));(attacks(Y,X), Y\=Z, Y\=X, grounded(Y)))).
