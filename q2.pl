1d4f9bae1515a95d0aa4187f9f0474523cf4fa5d
Jamal Khan <jzk09@doc.ic.ac.uk>
2013-02-19 16:07:02 +0000

    Question2 part 1 done again because Danish wanted to understand shit

--- q2.pl
+++ q2.pl
@@ -21,5 +21,7 @@ argument((C, [])) :- myRule(C,[]).
 argument((C, X)) :- myRule(C, LS), iterate(LS, X).
 
 iterate([], []).
-iterate([L|LS], [X|XS]) :- myAsm(X), X==L, iterate(LS, XS).
+iterate([L|LS], [X|XS]) :- myAsm(L), myAsm(X), X==L, iterate(LS, XS).
 iterate([L|LS], XS) :- myRule(L, YS), append(LS, YS, LS2), iterate(LS2, XS).
+
+
