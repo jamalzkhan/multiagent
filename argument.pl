7f15d01ae3409494b929a562c3e45098d327b19f
Alessandro Galli <ag6609@doc.ic.ac.uk>
2013-02-19 18:19:42 +0000

    new versions for the two functions in question 2

--- argument.pl
+++ argument.pl
@@ -1,3 +1,4 @@
+/*
 argument((C,X)) :- myAsm(C) , X = [C].
 argument((C,X)) :- myRule(C, []) , X = [].
 argument((C,X)) :- myRule(C, L1) , expand(L1,X).
@@ -5,4 +6,12 @@ argument((C,X)) :- myRule(C, L1) , expand(L1,X).
 expand([],[]).
 expand([H|T],R1) :- myAsm(H) , expand(T,R2) , R1 = [H|R2].
 expand([H|T],R1) :- myRule(H,L) , append(L,T,R2) , expand(R2,R1).
+*/
 
+argument((C, X)) :- myAsm(C), X=[C].
+argument((C, [])) :- myRule(C,[]).
+argument((C, X)) :- myRule(C, LS), iterate(LS, X).
+
+iterate([], []).
+iterate([L|LS], [X|XS]) :- myAsm(L), X=L, iterate(LS, XS).
+iterate([L|LS], XS) :- myRule(L, YS), append(LS, YS, LS2), iterate(LS2, XS).
--- attacks.pl
+++ attacks.pl
@@ -1,2 +1,6 @@
-attacks((C1,X1),(C2,X2)) :- argument((C1,X1)) , argument((C2,X2)) ,
-                            ( contrary(C2,C2) ; contrary(C3,C1) , member(C3,X2) ).
+%attacks((C1,X1),(C2,X2)) :- argument((C1,X1)) , argument((C2,X2)) ,
+%                            ( contrary(C2,C2) ; contrary(C3,C1) , member(C3,X2) ).
+
+attacks((CA,XA),(CB,XB)) :-
+	argument((CA,XA)), argument((CB,XB)),
+	member(X, XB), contrary(X, CA).
