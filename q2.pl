55f9f461438f05a8897a8fdedb6e043ddf05008c
Jamal Khan <jzk09@doc.ic.ac.uk>
2013-02-19 16:04:24 +0000

    Working q2 part 1

--- q2.pl
+++ q2.pl
@@ -3,11 +3,15 @@
 myAsm(a).
 myAsm(b).
 myAsm(c).
+myAsm(d).
+myAsm(e).
+myAsm(f).
+
 contrary(a,r).
 contrary(b,s).
 contrary(c,t).
-myRule(p,[a, r]).
-myRule(q,[]).
+myRule(p,[a, r, d, e]).
+myRule(q,[f]).
 myRule(r,[b,c, q]).
 
 
@@ -17,5 +21,5 @@ argument((C, [])) :- myRule(C,[]).
 argument((C, X)) :- myRule(C, LS), iterate(LS, X).
 
 iterate([], []).
-iterate([L|LS], [X|XS]) :- myAsm(L), L==X, iterate(LS, XS).
+iterate([L|LS], [X|XS]) :- myAsm(X), X==L, iterate(LS, XS).
 iterate([L|LS], XS) :- myRule(L, YS), append(LS, YS, LS2), iterate(LS2, XS).
