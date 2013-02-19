e549141428c5e97c9743e824a89e4b71b4617e04
Alessandro Galli <ag6609@doc.ic.ac.uk>
2013-02-19 19:21:28 +0000

    added couple of files

--- grounded.pl
+++ grounded.pl
@@ -1,14 +1,14 @@
-admissible(Y) :- not(att(Y,Y)), (not(att(X,Y)); (att(Y,Z), X==Z)).
+admissible(Y) :- \+(attacks(Y,Y)), (\+(attacks(X,Y)); (attacks(Y,Z), X==Z)).
 
-%forall(X,Y) :- \+(X,\+Y).
+forall(X,Y) :- \+ (X,\+Y).
 
-one_no_att :- arg(X), not(att(_,X)).
+one_no_attacks :- argument(X), \+(attacks(_,X)).
 
-grounded(Z) :-	one_no_att, !, arg(Z), forall((arg(X)), (\+(att(X,Z)); (att(Y,X), Y\=Z, Y\=X, grounded(Y)))).
+grounded(Z) :-	one_no_attacks, !, argument(Z), forall((argument(X)), (\+(attacks(X,Z)); (attacks(Y,X), Y\=Z, Y\=X, grounded(Y)))).
 grounded(Z) :-	Z = [].
 
-%defence([],R)		:-	findall(X, (arg(X), \+(att(_,X))), R).
-%defence([H|T],CF,R)	:-	findall(X, (arg(X), att(Y,X), att(H,Y), X\=Y,X\=H), L1),
+%defence([],R)		:-	findall(X, (argument(X), \+(attacks(_,X))), R).
+%defence([H|T],CF,R)	:-	findall(X, (argument(X), attacks(Y,X), attacks(H,Y), X\=Y,X\=H), L1),
 %						union(T,L1,L2), (L2==[] -> union([H],CF,R); (union([H],CF,Rap), defence(L2,Rap,R))).
 
 
