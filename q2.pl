070686063d87e14d834be42456d4a2b7ec5ed5ba
Jamal Khan <jzk09@doc.ic.ac.uk>
2013-02-19 15:44:25 +0000

    Question2 part 1 done

--- q2.pl
+++ q2.pl
@@ -6,23 +6,16 @@ myAsm(c).
 contrary(a,r).
 contrary(b,s).
 contrary(c,t).
-myRule(p,[r,a]).
+myRule(p,[a, r]).
 myRule(q,[]).
-myRule(r,[b,c]).
+myRule(r,[b,c, q]).
 
 
 %Case for argument(a, [b, c, a])
 argument((C, [X])) :- myAsm(C), C == X.
 argument((C, [])) :- myRule(C,[]).
-argument((C, [X | XS])) :- myRule(C, [Y | _]), myAsm(Y), X == Y, argument(C, XS).
+argument((C, X)) :- myRule(C, LS), iterate(LS, X).
 
-iterate(R, X | XS)
-
-argument((C, [X | XS])) :- myRule(C, [])
-
-
-
-
-%Base case for a claim
-argument((C, X)) :- myRule(C, X).
-%argument((C, X)) :- 
+iterate([], []).
+iterate([L|LS], [X|XS]) :- myAsm(L), L==X, iterate(LS, XS).
+iterate([L|LS], XS) :- myRule(L, YS), append(LS, YS, LS2), iterate(LS2, XS).
